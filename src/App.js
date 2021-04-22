9afe0c9b3ca7ead37455bdcbeb6ecdb7e97a0e32
Julian <tipler.julian@gmail.com>
2021-04-22 16:19:53 -0700

    second commit

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ const myTextField = (props) => {
   const [field, meta] = useField(props);
   const errorText = meta.error && meta.touched ? meta.error : "";
   return(
-    
+    <TextField {...field} helperText={errorText}/>
   )
 };
 
